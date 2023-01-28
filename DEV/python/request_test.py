2628c385befd367b6df8a64cf35e2982e6738532
Patrizio Cavarretta <78727572+drypatrick@users.noreply.github.com>
2023-01-28 10:35:09 +0100

    Test to get notify on changes

---
+++ DEV/python/request_test.py
@@ -0,0 +1,27 @@
+import requests
+r = requests.get('http://localhost:8668/v2/notify')
+
+url = 'http://localhost:8668/v2/notify'
+payload = {'some': 'data'}
+headers = {'content-type': 'application/json'}
+
+# r = requests.options(url, headers=headers)
+r = requests.post(url, headers=headers)
+# r = requests.get(url, headers=headers)
+
+# r = requests.post(url, data=json.dumps(payload), headers=headers)
+# r = requests.get(url, data=json.dumps(payload), headers=headers)
+
+# # r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
+print(r.status_code)
+# 200
+print(r.headers)
+
+# 'Content-Type: application/json', 'fiware-service: opcua_car', 'fiware-servicepath: /demo'
+# 'application/json; charset=utf8'
+print(r.encoding)
+# 'utf-8'
+print(r.text)
+# '{"authenticated": true, ...'
+# print(r.json())
+# {'authenticated': True, ...}
